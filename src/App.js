d75ab9f31e33f4aec1452ff215d80554867f00f6
duy ngeuyn <99794157+bunnidezi@users.noreply.github.com>
2022-04-09 15:08:27 +0700

    pushin P

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Container } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './componets/Header/Header'
+import SimpleBottomNavigation from './componets/MainNav';
+import Movies from './pages/Movies/Movies';
+import Search from './pages/Search/Search';
+import Series from './pages/Series/Series';
+import Trending from './pages/Trending/Trending';
+
+
+function App() {
+    
+  return (
+    <BrowserRouter>
+    <Header/> 
+    <div className='app'> 
+        <Container>
+            <Routes>
+                <Route path='/' element={<Trending/>} exact/>
+                <Route path='/movies' element={<Movies/>}/>
+                <Route path='/series' element={<Series/>}/>
+                <Route path='/search' element={<Search/>}/>
+            </Routes>
+            <div onClick={() =>window.scroll(0, 0)}
+        className="move">⬆</div>
+        </Container>
+    </div>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App
